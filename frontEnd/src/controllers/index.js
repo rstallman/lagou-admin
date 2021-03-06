6049e2f3e45fccef751345523188be43ba0be247
rstallman <sdustljl@163.com>
2021-03-06 10:47:43 +0800

    remove few bugs

--- frontEnd/src/controllers/index.js
+++ frontEnd/src/controllers/index.js
@@ -2,24 +2,20 @@ import indexTpl from "../views/index.art";
 import {auth as authModel} from '../models/auth'
 import img from '../assets/user.jpg'
 
-export const index = (router) => { 
-  
+export const index = (router) => {  
   return async (req, res, next) => {
     let result = await authModel();
     if(result.ret){ 
-
-      const html =  indexTpl({
-         subRouter:  res.subRoute (),
+      const html = indexTpl({
+         subRouter: res.subRoute(),
          img
       }); 
-      next(html)
-     
+      next(html)   
       // window resize,让页面撑满屏幕
-      $(window, ".wrapper").resize();
-     
-      const $as = $('#sidebar-menu li:not(:first-child) a') 
+      $(window, ".wrapper").resize();    
+      const $anchors = $('#sidebar-menu li:not(:first-child) a') 
       let hash = location.hash
-      $as
+      $anchors
         .filter(`[href="${hash}"]`)
         .parent()
         .addClass('active')
